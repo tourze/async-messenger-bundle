b08847491c7c780a8c7228531dc5129a266850e3
github-ci <you@example.com>
2025-06-14 13:54:33 +0800

    update

--- src/AsyncMessengerBundle.php
+++ src/AsyncMessengerBundle.php
@@ -2,8 +2,15 @@
 
 namespace Tourze\AsyncMessengerBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\AsyncMessengerBundle\DependencyInjection\RemoveUnusedServicePass;
 
 class AsyncMessengerBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $container->addCompilerPass(new RemoveUnusedServicePass());
+    }
 }
